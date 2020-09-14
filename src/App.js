a505981fc51b82189d8c288e9ded065493f1cf2a
awantech-io <hello.wanlab@gmail.com>
2020-09-14 17:24:19 +0800

    Todo component & TodoList componet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
+class App extends Component {
+  state = {
+    todos:[
+      {
+        id:1,
+        title: 'take out Thrash',
+        completed:false
+      },
+      {
+        id:2,
+        title: 'dinner with wife',
+        completed:false
+      },  
+      {
+        id:3,
+        title: 'meeting with client',
+        completed:false
+      }
+    ]
 
-    </div>
-  );
+  }
+  render () {
+    
+    return (
+      <div>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
